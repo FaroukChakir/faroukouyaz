4bb2c371c7eee039e44010f42f3a661d2a0060b7
FaroukChakir <56736005+FaroukChakir@users.noreply.github.com>
2021-07-04 19:31:18 +0100

    AA
    AA

--- IlearnProject/backend/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# admin.site.register(TeacherStudent)
-admin.site.register(TeacherStudent)
-admin.site.register(Courses)
-# Register your models here.
-
